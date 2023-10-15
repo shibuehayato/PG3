ecc8b6330a5bf940a1bde82cd0c9d7e6085ef7e4
shibuehayato <115055046+shibuehayato@users.noreply.github.com>
2023-10-15 12:24:31 +0900

    01_03完了

--- main.cpp
+++ main.cpp
@@ -1,23 +1,57 @@
 ﻿#include <stdio.h>
+#include <iostream>
+#include <Windows.h>
+#include <time.h>
 
-int Recursive(int num, int money) {
-	if (num <= 0)
+typedef void (*PFunc)(int,int);
+
+// コールバック関数
+void DiceResult(int result, int prediction)
+{
+	if (result == 1 || result == 3 || result == 5)
 	{
-		return (0);
+		if (prediction == 1)
+		{
+			printf("出目が%dで奇数だから正解です\n",result);
+		}
+		if (prediction == 2)
+		{
+			printf("出目が%dで奇数だから不正解です\n", result);
+		}
 	}
 
-	return (money + Recursive(num - 1, money * 2 - 50));
+	if (result == 2 || result == 4 || result == 6)
+	{
+		if (prediction == 1)
+		{
+			printf("出目が%dで偶数だから不正解です\n",result);
+		}
+		if (prediction == 2)
+		{
+			printf("出目が%dで偶数だから正解です\n", result);
+		}
+	}
+}
+
+void setTimeout(PFunc p, int result, int prediction)
+{
+	Sleep(3 * 1000);
+	p(result, prediction);
 }
 
 int main() {
-	int num = 8;
+	unsigned int currentTime = time(nullptr);
+	srand(currentTime);
 
-	int Normal = 1072 * num;
+	int result = rand() % 6 + 1 ;
 
-	int money = 100;
-	int result;
-	result = Recursive(num, money);
-	printf("一般的は%d時間働くと%d稼げる\n", num, Normal);
-	printf("再帰的は%d時間働くと%d稼げる\n", num,result);
+	int prediction;
+	printf("奇数なら1,偶数なら2を入力してください\n");
+	scanf_s("%d",&prediction);
+	PFunc p;
+	p = DiceResult;
+	setTimeout(p,result, prediction);
+
+
 	return 0;
 }
