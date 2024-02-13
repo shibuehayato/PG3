5fb25079afe6641c011dd949fe94ce0fd03c3728
shibuehayato <115055046+shibuehayato@users.noreply.github.com>
2024-02-13 21:17:46 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,29 +1,42 @@
 ﻿#include <stdio.h>
-#include <vector>
 #include <string>
 #include <algorithm>
 #include <iostream>
 #include <thread>
+#include <time.h>
+#include <Windows.h>
+#include <time.h>
 
 using namespace std;
 
-void Thread1() {
-	printf("thread 1\n");
-}
-void Thread2() {
-	printf("thread 2\n");
-}
-void Thread3() {
-	printf("thread 3\n");
-}
-
 int main()
 {
-	thread th1(Thread1);
-	th1.join();
-	thread th2(Thread2);
-	th2.join();
-	thread th3(Thread3);
-	th3.join();
+	Sleep(2 * 1000);
+
+	// コピーの前の時間を記録する
+	auto start = chrono::steady_clock::now();
+
+	string a(100000, 'a');
+
+	// 文字列のコピーを作成する
+	string copiedA = a;
+	// コピーの後の時間を記録する
+	auto end = chrono::steady_clock::now();
+	// 経過時間を計算する
+	auto elapsedTimeCopy = chrono::duration_cast<chrono::microseconds>(end - start);
+
+	// 移動の前の時間を記録する
+	start = chrono::steady_clock::now();
+	// 文字列の移動を作成する
+	string moveA = move(a);
+	// 移動の後の時間を記録する
+	end = std::chrono::steady_clock::now();
+	// 経過時間を計算する
+	auto elapsedTimeMove = chrono::duration_cast<chrono::microseconds>(end - start);
+	
+	cout << "100,000文字を移動とコピーで比較しました。\n";
+	cout << "コピー : " << elapsedTimeCopy.count() << "μs\n";
+	cout << "移動 : " << elapsedTimeMove.count() << "μs\n";
+
 	return 0;
 }
